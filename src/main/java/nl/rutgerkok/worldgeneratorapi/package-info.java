f0ea9ca32458593c742b2f54d9627d3b73f13905
mrburger <nicklekson@gmail.com>
2019-09-11 18:09:19 -0400

    Fixed mob spawning issues.

--- src/main/java/nl/rutgerkok/worldgeneratorapi/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Main package of the world generator API.
- */
-@ParametersAreNonnullByDefault
-package nl.rutgerkok.worldgeneratorapi;
-
-import javax.annotation.ParametersAreNonnullByDefault;
--- src/main/java/nl/rutgerkok/worldgeneratorapi/property/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Used for storing properties of worlds and biomes.
- *
- */
-@ParametersAreNonnullByDefault
-package nl.rutgerkok.worldgeneratorapi.property;
-
-import javax.annotation.ParametersAreNonnullByDefault;
